5dc64e78e521a95f73be8327a80862e670519dbd
Brad Breiten <jonathan@breiten.us>
2022-03-05 11:28:07 -0700

    Add webmock setup

---
+++ spec/support/webmock.rb
@@ -0,0 +1,10 @@
+# spec/support/webmock.rb
+
+# This line makes it so WebMock and RSpec know how to talk to each other.
+
+#require "webmock/rspec" #Comment this line back in
+
+# This line disables HTTP requests, with the exception of HTTP requests
+# to localhost.
+
+# WebMock.disable_net_connect!(allow_localhost: true) # Comment this line back in
