48321954fea922fd00b06a2ee6d29fb83dbadda8
Brad Breiten <jonathan@breiten.us>
2022-03-06 11:25:11 -0700

    Feat: OpenWeatherFacade parses the Service response to create weather objects

---
+++ app/poros/hourly_weather.rb
@@ -0,0 +1,13 @@
+class HourlyWeather
+  attr_reader :datetime,
+              :temp,
+              :conditions,
+              :icon
+
+  def initialize(data)
+    @datetime   = Time.at(data[:dt])
+    @temp       = data[:temp]
+    @conditions = data[:weather][0][:description]
+    @icon       = data[:weather][0][:icon]
+  end
+end
