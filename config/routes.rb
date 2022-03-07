eae4bacafd4479fe8ae9b19187afca8555faa85b
Brad Breiten <jonathan@breiten.us>
2022-03-07 09:52:22 -0700

    Add book poros

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: [:index]
       resources :backgrounds, only: [:index]
+      resources :book_search, only: [:index]
     end
   end
 end
