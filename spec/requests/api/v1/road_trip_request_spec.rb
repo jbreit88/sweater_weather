e7d17211aa9e61cf820ade328a5c790c3ab14aab
Brad Breiten <jonathan@breiten.us>
2022-03-08 13:42:50 -0700

    Add sad path testing

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -1,13 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe 'The Road Trip API' do
+RSpec.describe 'The Road Trip API', type: :request do
   describe 'create: post /api/v1/road_trip' do
     describe 'happy path' do
       it 'returns a json repsonse of road trip information' do
+        # Create a user
+        user_params = {
+          email: "whatever@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+
+        created_user = User.last
+
+        # Log user in when sessions work
+
+        # login_params = {
+        #   email: "whatever@example.com",
+        #   password: "password",
+        # }
+        #
+        # headers = {"CONTENT_TYPE" => "application/json"}
+        #
+        # post '/api/v1/sessions', headers: headers, params: JSON.generate(login_params)
+
+        # Make road trip
         road_trip_params = {
           "origin": "Denver,CO",
           "destination": "Pueblo,CO",
-          "api_key": "jgn983hy48thw9begh98h4539h4"
+          "api_key": created_user.api_key
         }
 
         headers = {"CONTENT_TYPE" => "application/json"}
@@ -32,6 +57,92 @@ RSpec.describe 'The Road Trip API' do
         expect(road_trip[:data][:attributes][:weather_at_eta]).to be_a Hash
         expect(road_trip[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
         expect(road_trip[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+      end
+    end
+
+    describe 'sad path' do
+      it 'returns a 401 error message when API key is invalid' do
+        # Create a user
+        user_params = {
+          email: "whatever@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+
+        created_user = User.last
+
+        # Log user in when sessions work
+
+        # login_params = {
+        #   email: "whatever@example.com",
+        #   password: "password",
+        # }
+        #
+        # headers = {"CONTENT_TYPE" => "application/json"}
+        #
+        # post '/api/v1/sessions', headers: headers, params: JSON.generate(login_params)
+
+        # Make road trip
+        road_trip_params = {
+          "origin": "Denver,CO",
+          "destination": "Pueblo,CO",
+          "api_key": 'Not User Api Key'
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post '/api/v1/road_trip', headers: headers, params: JSON.generate(road_trip_params)
+
+        error = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(401)
+        expect(error[:error][:exception]).to eq("You must provide a valid authentication key to access this feature.")
+      end
+
+      it 'returns a 400 error message when the route is impossible' do
+        # Create a user
+        user_params = {
+          email: "whatever@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+
+        created_user = User.last
+
+        # Log user in when sessions work
+
+        # login_params = {
+        #   email: "whatever@example.com",
+        #   password: "password",
+        # }
+        #
+        # headers = {"CONTENT_TYPE" => "application/json"}
+        #
+        # post '/api/v1/sessions', headers: headers, params: JSON.generate(login_params)
+
+        # Make road trip
+        road_trip_params = {
+          "origin": "London,UK",
+          "destination": "Pueblo,CO",
+          "api_key": created_user.api_key
+        }
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+
+        post '/api/v1/road_trip', headers: headers, params: JSON.generate(road_trip_params)
+
+        error = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(400)
+        expect(error[:error][:exception]).to eq("Cannot calculate route.")
       end
     end
   end
