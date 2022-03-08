faa33191f6dfc41bb4d480d00910cc6837d177b4
Brad Breiten <jonathan@breiten.us>
2022-03-08 11:10:51 -0700

    Add shoulda matchers config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -167,3 +167,10 @@ RSpec.configure do |config|
     stub_request(:get, "http://openlibrary.org/search.json?q=Denver,CO").to_return(body: books_response, status: 200)
   end
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
